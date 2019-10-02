b9a2719ebe7dd3ddc39cbeb0cb42b5a3b7a36cc3
Jamie <jaylovelace42@gmail.com>
2019-10-02 21:34:19 +0100

    initial react code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const HomePage = () => {
+
+  return (
+    <main>
+      <header>
+        EPISODIC
+      </header>
+      <body>
+        <input type="text" placeholder="searchbar here.." /><button>Button</button>
+        <div><small>an image here</small><p>with caption here</p></div>
+        <div><small>an image here</small><p>with caption here</p></div>
+        <div><small>an image here</small><p>with caption here</p></div>
+      </body>
+    </main>
+  )
+}
+
+export default HomePage;
